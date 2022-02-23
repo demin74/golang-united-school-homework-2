94047647087c5e55e7f8137455a3378a60813bba
Pashka <demin74@list.ru>
2022-02-23 13:44:13 +0300

    new type

--- solution.go
+++ solution.go
@@ -1,5 +1,6 @@
 package square
 
+type countSides int
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +9,5 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+func CalcSquare(sideLen float64, sidesNum countSides) float64 {
 }
