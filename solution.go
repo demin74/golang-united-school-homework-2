a68d51ae11f3678f78d494cf850fa8b40329c7df
Pashka <demin74@list.ru>
2022-02-23 17:58:57 +0300

    PreVersion

--- solution.go
+++ solution.go
@@ -1,19 +1,40 @@
 package square
 
+import "math"
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 type CountSides int
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
 const (
- 		SidesTriangle 	CountSides = 3
-		SidesSquare		CountSides = 4
-		SidesCircle		CountSides = 0
+ 		SidesCircle		CountSides 	= iota    //0
+ 		SidesTriangle 				= iota+2  //3=1+2
+		SidesSquare					= iota+2  //4=2+2
 	  )
 
+var Q int64=0
+var Qf float64=0
 // it's like:
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
 func CalcSquare(sideLen float64, sidesNum CountSides) float64 {
+///////////////////////////////////////////////////////////////////////////////
+	Q:=sideLen*sideLen // квадрат числа (возведение в вторую степень - число степени два и т.д. и т.п.)
+	 Qf:=float64(Q)
+
+	if sidesNum=4 {
+	 return Qf
+	}
+	else if sidesNum=3 {
+	 return Qf*(math.Sqrt(3)/4)
+	}
+	else if sidesNum=0 {
+	 return Qf*math.Pi
+	}
+	else {
+	 return 0
+	}
+///////////////////////////////////////////////////////////////////////////////	
 }
